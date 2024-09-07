e85276f41cee814d2173948993a93ef0d4a0f26a
Hash-Ezz <ezz.alhashedi@gmail.com>
2024-09-08 01:58:06 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero"
+
+function App() {
+
+  return (
+     <div className="bg-gray-300 p-0 m-0 font-sans">
+        <Navbar />
+        <Hero />
+     </div>
+  )
+}
+
+export default App
